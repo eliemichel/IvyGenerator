c9da530bdd4e8521e53c2d6ad71036a0a1a1fc45
Elie Michel <elie.michel@ens.fr>
2017-10-27 00:35:44 +0200

    Reorganize project folder layout

--- build-msvc15.bat
+++ build-msvc15.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DQt5Widgets_DIR=C:\Qt\Qt5.9.2\5.9.2\msvc2017_64\lib\cmake\Qt5Widgets -DQt5OpenGL_DIR=C:\Qt\Qt5.9.2\5.9.2\msvc2017_64\lib\cmake\Qt5OpenGL
+cmake .. -G "Visual Studio 15 2017 Win64" -DQt5Widgets_DIR=E:\Qt\Qt5.9.2\5.9.2\msvc2017_64\lib\cmake\Qt5Widgets -DQt5OpenGL_DIR=E:\Qt\Qt5.9.2\5.9.2\msvc2017_64\lib\cmake\Qt5OpenGL
